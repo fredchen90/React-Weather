d83606ccf5c0ae959a376ee30f72438b61a106bb
fredchen90 <fredchen90@gmail.com>
2017-07-18 22:35:42 +0800

    Add axios for api request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '5df396cf7b4d7b339bc250ebfd041f2e';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
