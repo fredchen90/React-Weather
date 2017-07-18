09f3ef2c4489eba0576fc745736bc92890249036
fredchen90 <fredchen90@gmail.com>
2017-07-18 23:57:13 +0800

    Build a basic searchbar api request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '5df396cf7b4d7b339bc250ebfd041f2e';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
