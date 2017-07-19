c03c4283cf760240c2a4199654f9b1be12eed4b6
fredchen90 <fredchen90@gmail.com>
2017-07-19 23:45:02 +0800

    Create reusable chart component.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map((weather) => weather.main.temp);
+		const pressures = cityData.list.map((weather) => weather.main.pressure);
+		const humidities = cityData.list.map((weather) => weather.main.humidity);
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td>
+					<Chart data={temps} color="orange" />
+				</td>
+				<td>
+					<Chart data={pressures} color="blue" />
+				</td>
+				<td>
+					<Chart data={humidities} color="red" />
+				</td>
+			</tr>
+		);
+	}
 	render() {
-		console.log(this.props.weather);
 		return (
 			<table className="table table-hover">
 				<thead>
@@ -15,6 +35,7 @@ class WeatherList extends Component {
 					</tr>
 				</thead>
 				<tbody>
+					{this.props.weather.map(this.renderWeather)}
 				</tbody>
 			</table>
 		);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index';
 
+// state is pre-date, action is new data
 export default function(state = [], action) {
 	switch(action.type) {
 		case FETCH_WEATHER:
